4fdb709dc31a6bdb1bb155081238e1a26849dcb4
dabidesh <dabidesh@gmail.com>
2021-04-12 12:17:22 +0300

    small change

--- user.js/linkFromEmail.user.js
+++ user.js/linkFromEmail.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Link from email
-// @namespace    http://tampermonkey.net/
+// @namespace    https://rekontra.net/
 // @version      0.1
 // @description  Insert link from email
-// @author       You
+// @author       dabidesh
 // @match        *
 // @include      *
-// @icon         https://www.google.com/s2/favicons?domain=podove.biz
+// @icon         https://www.google.com/s2/favicons?domain=rekontra.net
 // @noframe
 // @grant        none
 // ==/UserScript==
@@ -18,10 +18,9 @@
   [...aElements].forEach(a => {
     let href = a.href.split(':');
     if (href[0] == 'mailto') {
-      //console.log('Имаме имейл');
       let [_, url] = a.href.split('@');
-      url = url.split('?');
-      a.insertAdjacentHTML('afterend', `<a href="https://${url[0]}"> [link] </a>`);
+      let [urlClean, __] = url.split('?');
+      a.insertAdjacentHTML('afterend', `<a href="https://${urlClean}"> [link ${urlClean}] </a>`);
     }
   });
 })();
