f90a96ce83633c47b79e24163bb959693c24bd64
dabidesh <dabidesh@gmail.com>
2023-04-10 19:58:46 +0300

    title

--- js/boilerPlateCode/app.js
+++ js/boilerPlateCode/app.js
@@ -13,26 +13,23 @@ button.onclick = () => {
 
 button0.onclick = () => {
   let arr = document.getElementById('in0').value.split('\n');
-  let selectName = (arr.shift()).trim();
-  let data = (arr.shift()).trim();
+  arr = arr
+    .map(e => e.trim)
+    .filter(e => e != '');
+  let selectName = arr.shift();
+  let data = arr.shift();
 
   let str = 'let o = {}\n';
   for (let i = 0; i < arr.length; i++) {
-    arr[i] = arr[i].trim();
-    if (arr[i] != '') {
-      str += `if (req.body.${selectName} == '${arr[i]}') o.${selectName}${arr[i]} = true;\n`;
-
-    }
-    let strHBS = '';
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] != '') {
-        strHBS += `{{#if ${data}.o.${selectName}${arr[i]}}}selected{{/if}}\n`;
-      }
-    }
-    let elOut = document.getElementById('outC');
-    elOut.value = str;
-    elOut.select();
-    document.execCommand('copy');
-    document.getElementById('outHBS').value = strHBS;
+    str += `if (req.body.${selectName} == '${arr[i]}') o.${selectName}${arr[i]} = true;\n`;
   }
+  let strHBS = '';
+  for (let i = 0; i < arr.length; i++) {
+    strHBS += `{{#if ${data}.o.${selectName}${arr[i]}}}selected{{/if}}\n`;
+  }
+  let elOut = document.getElementById('outC');
+  elOut.value = str;
+  elOut.select();
+  document.execCommand('copy');
+  document.getElementById('outHBS').value = strHBS;
 };
