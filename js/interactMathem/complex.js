c990d8c1d65418c48237a43220b1f0ec826280fb
dabidesh <dabidesh@gmail.com>
2024-04-03 21:59:56 +0300

    complex numbers expressions

---
+++ js/interactMathem/complex.js
@@ -0,0 +1,46 @@
+'use strict';
+// @ts-nocheck
+let p = console.log;
+
+getExpressions.onclick = () => {
+
+  let str = inputComplexNumbers.value;
+  let arrayComplexNumbers = [];
+  for (let i = 0, j = 0; i < str.length; i++) {
+    if (!isNaN(parseInt(str[i])) && str[i - 1] != 'z') {
+      arrayComplexNumbers[j] = str[i];
+      while (!isNaN(parseInt(str[i + 1]))) {
+        arrayComplexNumbers[j] += str[i + 1];
+        i++;
+      }
+      j++;
+    }
+  }
+
+  p(arrayComplexNumbers);
+
+  let z1 = '(' + arrayComplexNumbers[0].toString() + '+' +
+    arrayComplexNumbers[1].toString() + 'i)';
+  let z2 = '(' + arrayComplexNumbers[2].toString() + '+' +
+    arrayComplexNumbers[3].toString() + 'i)';
+
+  p(z1, z2);
+
+  outZ1Z2MinusZ1PlusZ2.value = z1 + '*' + z2 + '-' + '(' + z1 + '+' + z2 + ')';
+  outZ1Z2MinusZ1PlusZ2.style.width = outZ1Z2MinusZ1PlusZ2.value.length + 'ch';
+  outZ1DivideZ2.value = z1 + '/' + z2;
+  outZ1DivideZ2.style.width = outZ1DivideZ2.value.length + 'ch';
+};
+
+main.onclick = (e) => {
+  //preventDefault(e);
+  if (e.target.tagName == 'BUTTON' && e.target.classList.contains('copy')) {
+    const inputEl = e.target.previousElementSibling;
+    copyId(inputEl);
+  }
+};
+
+function copyId(el) {
+  el.select();
+  document.execCommand('copy');
+}
