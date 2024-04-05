8329f8f688978a143d5bd3562509a4b85d25e257
dabidesh <dabidesh@gmail.com>
2024-04-05 21:48:55 +0300

    z1 z2 z3

--- js/interactMathem/complex.js
+++ js/interactMathem/complex.js
@@ -23,9 +23,11 @@ getExpressions.onclick = () => {
     arrayComplexNumbers[1].toString() + ')' + 'i)';
   let z2 = '(' + arrayComplexNumbers[2].toString() + '+' + '(' +
     arrayComplexNumbers[3].toString() + ')' + 'i)';
-  let z3 = '(' + arrayComplexNumbers[4].toString() + '+' + '(' +
-    arrayComplexNumbers[5].toString() + ')' + 'i)';
-
+  let z3 = 'нЕма';
+  if (arrayComplexNumbers[4] != undefined && arrayComplexNumbers[5] != undefined) {
+    z3 = '(' + arrayComplexNumbers[4].toString() + '+' + '(' +
+      arrayComplexNumbers[5].toString() + ')' + 'i)';
+  }
   p(z1, z2);
 
   outZ1Z2MinusZ1PlusZ2.value = z1 + '*' + z2 + '-' + '(' + z1 + '+' + z2 + ')';
@@ -34,6 +36,9 @@ getExpressions.onclick = () => {
   outZ1DivideZ2.style.width = outZ1DivideZ2.value.length + 'ch';
   outZ1MultipleZ2MultipleZ3.value = z1 + '*' + z2 + '*' + z3;
   outZ1MultipleZ2MultipleZ3.style.width = outZ1MultipleZ2MultipleZ3.value.length + 'ch';
+
+  outZ1Z2Z3.value = z1 + ' ' + z2 + ' ' + z3;
+  outZ1Z2Z3.style.width = outZ1Z2Z3.value.length + 'ch';
 };
 
 main.onclick = (e) => {
